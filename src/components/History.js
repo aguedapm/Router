214f3201251f8ca602646b1cf28eb2231949b87f
Agueda Perez <aguedapm@yahoo.com>
2019-04-15 14:34:39 +0200

    Router quest solved

---
+++ src/components/History.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const History = () => (
+  <p>Notre histoire</p>
+);
+
+export default History;
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => (
+  <p>Bienvenue sur l'accueil</p>
+);
+
+export default Home;
